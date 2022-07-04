9b973e66d82382217c35af8a349bc82d34a9ff53
Claudio Puggioni <claudieddu@hotmail.com>
2022-07-04 12:08:08 -0700

    first draft

---
+++ src/components/Board.jsx
@@ -0,0 +1,25 @@
+import Square from "./Square.jsx";
+
+function Board(props) {
+  const generateRow = (rowNum) => (
+    <div className="row">
+      {props.currentBoardPassed[rowNum].map((el, idx) => (
+        <Square
+          updateBoard={() => {
+            props.handleClick(rowNum, idx);
+          }}
+          id={"r" + rowNum + "c" + idx}
+          value={props.currentBoardPassed[rowNum][idx]}
+        />
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="boardPlusMenu">
+      <div className="board">{props.currentBoardPassed.map((el, idx) => generateRow(idx))}</div>
+    </div>
+  );
+}
+
+export default Board;
---
+++ src/components/Game.jsx
@@ -0,0 +1,97 @@
+import Board from "./Board";
+import { useState } from "react";
+const Osound = new Audio("assets/Osound.mp3");
+const Xsound = new Audio("assets/Xsound.mp3");
+
+let startingBoard = new Array(3).fill(0).map((el) => new Array(3).fill(" "));
+let boardHistory = [];
+
+function Game() {
+  let [currBoard, setCurrentBoard] = useState(startingBoard);
+  let [isXPlaying, setIsXPlaying] = useState(true);
+  let [gameStatus, setGameStatus] = useState("Starting...");
+  let [history, setHistory] = useState(boardHistory);
+
+  function ticTacToeGameStatus(board, player) {
+    function hasPlayerWon(player) {
+      if (board[0][0] === player && board[0][1] === player && board[0][2] === player) {
+        return true;
+      } else if (board[1][0] === player && board[1][1] === player && board[1][2] === player) {
+        return true;
+      } else if (board[2][0] === player && board[2][1] === player && board[2][2] === player) {
+        return true;
+      } else if (board[0][0] === player && board[1][0] === player && board[2][0] === player) {
+        return true;
+      } else if (board[0][1] === player && board[1][1] === player && board[2][1] === player) {
+        return true;
+      } else if (board[0][2] === player && board[1][2] === player && board[2][2] === player) {
+        return true;
+      } else if (board[0][0] === player && board[1][1] === player && board[2][2] === player) {
+        return true;
+      } else if (board[0][2] === player && board[1][1] === player && board[2][0] === player) {
+        return true;
+      }
+      return false;
+    }
+
+    if (hasPlayerWon(player)) {
+      return player === "X" ? "Player X has won" : "Player O has won";
+    } else if (board[0].includes(" ") || board[1].includes(" ") || board[2].includes(" ")) {
+      return "Game in Progress";
+    } else {
+      return "Draw";
+    }
+  }
+
+  let handleClick = (row, col) => {
+    if (gameStatus.slice(gameStatus.length - 3) !== "won" && gameStatus !== "Draw") {
+      let copyBoard = JSON.parse(JSON.stringify(currBoard));
+
+      copyBoard[row][col] = isXPlaying ? "X" : "O";
+      isXPlaying ? Xsound.play() : Osound.play();
+
+      let temp = history.slice();
+      temp.push({ board: copyBoard, currentPlayer: isXPlaying });
+      setHistory(temp);
+
+      setCurrentBoard(copyBoard);
+      setIsXPlaying(!isXPlaying);
+
+      setGameStatus(ticTacToeGameStatus(copyBoard, isXPlaying ? "X" : "O"));
+    }
+  };
+
+  let somefunction = (index) => {
+    setCurrentBoard(history[index].board);
+    setIsXPlaying(!history[index].currentPlayer);
+    history = history.slice(0, index + 1);
+    setHistory(history);
+    if (gameStatus.slice(gameStatus.length - 3) === "won" || gameStatus === "Draw") setGameStatus("Game in Progress");
+  };
+
+  return (
+    <div id="gameContainer">
+      <div id="gameColumn">
+        <h1>Disrespectful</h1> <h1>TicTacToe</h1>
+        <div id="currentPlayerHeader">
+          Current Player:<div id="currentPlayerXO">{isXPlaying ? "X" : "O"}</div>
+        </div>
+        <Board currentBoardPassed={currBoard} handleClick={handleClick} />
+        <div id="progress">Status: {gameStatus}</div>
+      </div>
+
+      <div id="historyColumn">
+        <div id="historyHeader">History</div>
+        {history.map((ele, idx) => {
+          return (
+            <button className="historyButtons" onClick={() => somefunction(idx)}>
+              Move #{idx + 1}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Game;
